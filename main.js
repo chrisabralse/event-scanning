5c47a71edd1f4eb70f85f09f4fdec73f23ff8e06
ChrisAbra <chrisabra@live.com>
2019-01-31 22:36:23 +0000

    added 404 and database to store values

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const path = require('path');
+var Datastore = require('nedb'),
+	devices = new Datastore({
+		filename: path.join(__dirname + '/databases/devices'),
+		autoload: true
+	});
 let port = process.env.PORT || 8080;
 
 const app = express();
@@ -8,6 +14,31 @@ app.listen(port, function () {
 	console.log('server up on port: ' + port);
 });
 
+app.use(express.static('statics'))
+
 app.get('/', function (req, res) {
-	res.sendFile('site/index.html');
+	res.sendFile(path.join(__dirname + '/sites/index.html'));
+	console.log(devices);
+});
+
+
+app.get('*', function (req, res) {
+	res.status(404);
+
+	// respond with html page
+	if (req.accepts('html')) {
+		res.sendFile(path.join(__dirname + '/sites/404.html'));
+		return;
+	}
+
+	// respond with json
+	if (req.accepts('json')) {
+		res.send({
+			error: 'Not found'
+		});
+		return;
+	}
+
+	// default to plain-text. send()
+	res.type('txt').send('Not found');
 });
---
+++ statics/QRwrapper.js
@@ -0,0 +1,102 @@
+let codesScannedTime = {};
+
+let video = document.createElement("video");
+let canvasElement = document.getElementById("canvas");
+let canvas = canvasElement.getContext("2d");
+let loadingMessage = document.getElementById("loadingMessage");
+
+function drawLine(begin, end, color) {
+	canvas.beginPath();
+	canvas.moveTo(begin.x, begin.y);
+	canvas.lineTo(end.x, end.y);
+	canvas.lineWidth = 4;
+	canvas.strokeStyle = color;
+	canvas.stroke();
+}
+
+// Use facingMode: environment to attemt to get the front camera on phones
+navigator.mediaDevices
+	.getUserMedia({
+		video: {
+			facingMode: "environment"
+		}
+	})
+	.then(function (stream) {
+		video.srcObject = stream;
+		video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+		video.play();
+		requestAnimationFrame(tick);
+	});
+
+function tick() {
+	loadingMessage.innerText = "⌛ Loading video...";
+	if (video.readyState === video.HAVE_ENOUGH_DATA) {
+		loadingMessage.hidden = true;
+		canvasElement.hidden = false;
+
+		canvasElement.height = video.videoHeight;
+		canvasElement.width = video.videoWidth;
+		canvas.drawImage(
+			video,
+			0,
+			0,
+			canvasElement.width,
+			canvasElement.height
+		);
+		let imageData = canvas.getImageData(
+			0,
+			0,
+			canvasElement.width,
+			canvasElement.height
+		);
+		let code = jsQR(imageData.data, imageData.width, imageData.height, {
+			inversionAttempts: "dontInvert"
+		});
+		if (code) {
+			drawLine(
+				code.location.topLeftCorner,
+				code.location.topRightCorner,
+				"#006400"
+			);
+			drawLine(
+				code.location.topRightCorner,
+				code.location.bottomRightCorner,
+				"#006400"
+			);
+			drawLine(
+				code.location.bottomRightCorner,
+				code.location.bottomLeftCorner,
+				"#006400"
+			);
+			drawLine(
+				code.location.bottomLeftCorner,
+				code.location.topLeftCorner,
+				"#006400"
+			);
+			document.body.dispatchEvent(
+				new CustomEvent("qrScanEvent", {
+					detail: code
+				})
+			);
+		}
+	}
+	requestAnimationFrame(tick);
+}
+
+document.body.addEventListener("qrScanEvent", function (e) {
+	if (codesScannedTime[e.detail.data]) {
+		let timeSinceLastScan = Math.abs(
+			new Date() - codesScannedTime[e.detail.data]
+		);
+		if (timeSinceLastScan > 5000) {
+			registerScan(e.detail.data);
+		}
+	} else {
+		registerScan(e.detail.data);
+	}
+});
+
+function registerScan(data) {
+	codesScannedTime[data] = new Date();
+	console.log(data);
+}
